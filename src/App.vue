40322a2ebd4d0d999a4c0581cf6101b3da10cec5
devseop <cocc263@gmail.com>
2023-12-14 22:45:40 +0900

    :construction: Wip

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import SignIn from './components/SignIn.vue'
+</script>
+
+<template>
+  <main><SignIn /></main>
+</template>
+
+<style lang="scss"></style>
---
+++ src/components/SignIn.vue
@@ -0,0 +1,37 @@
+<script setup lang='ts'>
+import { reactive } from 'vue'
+
+interface ISignInForm {
+  email: string;
+  password: string;
+}
+
+const signInForm = reactive<ISignInForm>({
+  email: '',
+  password: '',
+})
+
+function handleSignIn()  {
+  console.log(signInForm)
+  alert('welcome!')
+}
+
+</script>
+
+<template>
+  <form @submit.prevent='handleSignIn'>
+    <div>
+      <label>email</label>
+      <input v-model='signInForm.email' type='text' placeholder='email@example.com' />
+    </div>
+    <div>
+      <label>password</label>
+      <input v-model='signInForm.password' type='password' placeholder='password' />
+    </div>
+    <button type='submit'>SIGN IN</button>
+  </form>
+</template>
+
+<style lang='scss'>
+
+</style>
